3b28be97e6f20b4692da6af612741febd8ce991f
alionafomenko <a.fomenko2005@gmail.com>
2023-10-16 18:16:12 +0300

    lab_6

--- Lab_5/js/rectangle.js
+++ Lab_5/js/rectangle.js
@@ -5,14 +5,16 @@ function calculateRectangle() {
     const width = parseFloat(document.getElementById('width').value);
 
     if (!isNaN(length) && !isNaN(width)) {
-        const perimeter = 2 * (length + width);
-        const area = length * width;
-        const diagonal = Math.sqrt(length ** 2 + width ** 2);
+        if (width >=0 && length>=0){
+            const perimeter = 2 * (length + width);
+            const area = length * width;
+            const diagonal = Math.sqrt(length ** 2 + width ** 2);
 
-        document.getElementById('perimeter').textContent = `Периметр: ${perimeter}`;
-        document.getElementById('area').textContent = `Площа: ${area}`;
-        document.getElementById('diagonal').textContent = `Довжина діагоналі: ${diagonal}`;
-    } else {
+            document.getElementById('perimeter').textContent = `Периметр: ${perimeter}`;
+            document.getElementById('area').textContent = `Площа: ${area}`;
+            document.getElementById('diagonal').textContent = `Довжина діагоналі: ${diagonal}`;
+        }
+         } else {
         alert('Будь ласка, введіть числові значення для довжини та ширини.');
     }
 }
