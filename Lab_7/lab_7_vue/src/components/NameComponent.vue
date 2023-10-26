415d8206cacfb4f0469d6ee24d25956ea6d43502
alionafomenko <a.fomenko2005@gmail.com>
2023-10-26 11:58:28 +0300

    + 2 comp

---
+++ Lab_7/lab_7_vue/src/components/NameComponent.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div class="input-box">
+        <input type="text"
+               placeholder="Ім'я"
+               required v-on:input="checkName">
+    </div>
+    <div class="invalid" v-html="name_error"></div>
+
+</template>
+
+<script>
+    export default {
+        name: 'NameComponent',
+        components: {},
+        data(){
+            return{
+                name_error: '',
+            }
+        },
+        methods: {
+            checkName(event) {
+                function validateName(name) {
+                    let error = "";
+                    if (/\s+/.test(name.trim())){
+                        error += "Не має бути пробілів<br/>";
+                    }
+                    if (name.length > 15){
+                        error += "Має бути до 15 символів<br/>";
+                    }
+                    if (/[A-Z]+/.test(name) || /[a-z]+/.test(name)){
+                        error += "Не має бути латинських літер<br/>";
+                    }
+                    return error;
+
+                }
+
+                const name = event.target.value;
+
+                this.name_error = validateName(name);
+            },
+        }
+    }
+</script>
